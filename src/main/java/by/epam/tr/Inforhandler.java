cf50f231649b5f18e6fe675529e9c6061c352c9a
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-04-03 23:56:15 +0300

    Add files via upload

---
+++ src/main/java/by/epam/tr/Inforhandler.java
@@ -0,0 +1,10 @@
+package by.epam.tr;
+
+import by.epam.tr.view.ApplicationView;
+
+public class Inforhandler {
+  public static void main(String[] args) {
+    ApplicationView applicationView = new ApplicationView();
+    applicationView.applicationMenu();
+  }
+}
